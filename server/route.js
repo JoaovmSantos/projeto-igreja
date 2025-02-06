e6fa0680e996d65f0868597ebb9a844aa657f3d9
João Victor Martins dos Santos <108280920+JoaovmSantos@users.noreply.github.com>
2025-02-06 16:04:19 -0300

    Correções na insrção no banco de dados

--- server/route.js
+++ server/route.js
@@ -59,32 +59,61 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.get('/cadastro', (req, res) => {
+    res.render('cadastro');
+});
+
+router.post('/cadastro', (req, res) => {
+    const { nome, cargo, usuario, senha } = req.body;
+
+    if (!nome || !usuario || !senha) {
+        return res.status(400).send('Nome, usuário e senha são obrigatórios.');
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const senhaCriptografada = bcrypt.hashSync(senha, salt);
+
+    db.query(
+        'INSERT INTO pastor (nome, cargo, usuario, senha) VALUES (?, ?, ?, ?)',
+        [nome.trim(), cargo?.trim() | 'Sem Cargo', usuario.trim(), senhaCriptografada],
+        (err) => {
+            if (err) {
+                console.error('Erro ao cadastrar pastor:', err);
+                return res.status(500).send('Erro ao realizar o cadastro. Verifique se o usuário já existe.');
+            }
+            res.redirect('/login');
+        }
+    )
+});
+
+
 router.get('/pedidoOracao', (req, res) => {
     res.render('add_pedido');
 });
 
 
 router.post('/pedidoOracao', (req, res) => {
-    const { nome, beneficiado, categoria, descricao } = req.body;
+    const { nome, beneficiado, categoria } = req.body;
 
-    const palavraCount = descricao.trim().split(/\s+/).length;
-    if (palavraCount > 30) {
-        return res.status(400).send('O pedido deve conter no máximo 30 palavras.');
+    if (!nome) {
+        return res.status(400).send('O campo nome é obrigatório.');
     }
 
+    const categoriaFinal = categoria?.trim() || 'Geral';
+
     db.query(
-        'INSERT INTO pedidos (nome, beneficiado, descricao, categoria, lido, data_pedido) VALUES (?, ?, ?, ?, ?, NOW())',
-        [nome, beneficiado || nome, descricao.trim(), categoria, false],
+        'INSERT INTO pedidos (nome, beneficiado, categoria, lido, data_pedido) VALUES (?, ?, ?, ?, NOW())',
+        [nome.trim(), beneficiado?.trim() || nome.trim(), categoriaFinal, false],
         (err) => {
             if (err) {
-                console.error(err);
-                return res.status(500).send('Erro ao adicionar o pedido de oração');
+                console.error('Erro ao adicionar o pedido de oração:', err);
+                return res.status(500).send('Erro ao adicionar o pedido de oração.');
             }
             res.redirect('/pedidoOracao');
         }
     );
-    
 });
+
 
 router.get('/pedidos', verificarAutenticacao, (req, res) => {
     db.query('SELECT * FROM pedidos WHERE lido = FALSE', (err, results) => {
